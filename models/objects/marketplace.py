1c9d8019008ae8517bdd786541db778657f7166f
Sly <darkinleviathan@gmail.com>
2023-04-28 12:40:53 +0100

    Some updates and changes

--- models/objects/marketplace.py
+++ models/objects/marketplace.py
@@ -1,12 +1,72 @@
-from beanie import Document
+from beanie import Document, Link
 from pydantic import BaseModel, Field
 from enum import Enum
 from uuid import UUID, uuid4
 from datetime import datetime
+from typing import Optional
+from .user import User
+import re
+
+
+get_category = re.compile(r".*?\/(.*?)\/.*")
+
+
+blacklist = [
+    None,
+    "banner",
+    "companion",
+    "currency",
+    "egg",
+    "events",
+    "equipment",
+    "vendor",
+    "npc",
+    "hub",
+    "bottleworld",
+    "blueprint",
+    "gem",
+    "fixture",
+    "quest",
+    "reliquary",
+    "rewardcrate",
+    "Geode Mining Block",
+    "hastener",
+    "holiday",
+    "influencer",
+    "key",
+    "matchmaker_delve_dial_depth_interactable",
+    "Mining Block",
+    "terraforming",
+    "tutorial",
+    "test",
+]
+
+
+class Item(BaseModel):
+    identifier: str
+    name: str
+    description: str
+    icon: str
+
+    @property
+    def icon_url(self):
+        return "https://trovesaurus.com/data/catalog/" + self.icon
+
+    @property
+    def category(self):
+        r = get_category.match(self.identifier)
+        return r.group(1) if r else None
+
+    @property
+    def is_valid(self):
+        if self.category in blacklist:
+            return False
+        return not "notrade" in self.identifier.lower()
 
 
 class ListingStatus(Enum):
     listed = "Listed"
+    pending = "Pending"
     sold = "Sold"
     expired = "Expired"
     cancelled = "Cancelled"
@@ -14,8 +74,16 @@ class ListingStatus(Enum):
 
 class Listing(Document):
     uuid: UUID = Field(default_factory=uuid4)
+    item: str
+    seller: Link[User]
+    buyer: Optional[Link[User]] = None
     price: int
-    price_per: float
     amount: int
     status: ListingStatus = ListingStatus.listed
-    datetime: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    rating: Optional[int] = None
+    finished: Optional[datetime] = None
+
+    @property
+    def price_per(self):
+        return round(self.price / self.amount, 2)
