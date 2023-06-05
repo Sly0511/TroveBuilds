aa95c2df1ef2a55fe731d8d08c19291dd17f76cf
Sly <darkinleviathan@gmail.com>
2023-06-05 23:43:55 +0100

    Updated home page and added Cranny rotation

--- controllers/home.py
+++ controllers/home.py
@@ -7,27 +7,26 @@ import requests
 from flet import (
     Text,
     Column,
-    Row,
     Container,
     Card,
     ResponsiveRow,
     Image,
     Stack,
-    Divider,
     TextStyle,
-    BoxShadow,
-    TextSpan,
     LinearGradient,
     alignment,
     Tooltip,
     Border,
     BorderSide,
-    BlendMode
+    BlendMode,
+    Row,
+    VerticalDivider
 )
 from pytz import UTC
 
 from models.objects import Controller
 from utils import tasks
+from utils.data import Cranny
 
 
 class Widget(Container):
@@ -94,6 +93,7 @@ class HomeController(Controller):
                                 controls=[
                                     Image(
                                         color="black",
+                                        color_blend_mode=BlendMode.SATURATION,
                                         src_base64=base64.b64encode(
                                             requests.get(v["banner"]).content
                                         ).decode("utf-8")
@@ -161,6 +161,7 @@ class HomeController(Controller):
                                 controls=[
                                     Image(
                                         color="black",
+                                        color_blend_mode=BlendMode.SATURATION,
                                         src_base64=base64.b64encode(
                                             requests.get(v["banner"]).content
                                         ).decode("utf-8")
@@ -204,6 +205,76 @@ class HomeController(Controller):
                 for k, v in self.weekly_data.items()
             ],
         )
+        cranny = Cranny()
+        self.cranny_widgets = ResponsiveRow(
+            controls=[
+                Widget(
+                    controls=[
+                        Tooltip(
+                            message=(
+                                "\n".join(
+                                    [
+                                        "From " + start.strftime("%B %d, %Y") + " to " + end.strftime("%B %d, %Y")
+                                        for start, end in item.weeks
+                                    ]
+                                )
+                            ),
+                            content=Container(
+                                data=item,
+                                content=Column(
+                                    controls=[
+                                        Text(item.name + (" Ally" if item.ally else " Mount"), size=18, text_align="center"),
+                                        Row(
+                                            controls=[
+                                                *(
+                                                    [
+                                                        Text("Price"),
+                                                        VerticalDivider(),
+                                                        Image(
+                                                            "assets/images/resources/{}.png".format(
+                                                                "currency_points"
+                                                                if item.currency == "cubits" else
+                                                                "currency_credits"
+                                                            ),
+                                                            width=24
+                                                        ),
+                                                        Text(item.price)
+                                                    ] if item.currency is not None else
+                                                    [
+                                                        Text("Reward")
+                                                    ]
+                                                )
+                                            ]
+                                        )
+                                    ]
+                                ),
+                                border_radius=5,
+                                border=Border(
+                                    BorderSide(width=1, color="#cccccc"),
+                                    BorderSide(width=1, color="#cccccc"),
+                                    BorderSide(width=1, color="#cccccc"),
+                                    BorderSide(width=1, color="#cccccc")
+                                )
+                            ),
+                            border_radius=10,
+                            bgcolor="#1E1E28",
+                            text_style=TextStyle(color="#cccccc"),
+                            border=Border(
+                                BorderSide(width=2, color="#cccccc"),
+                                BorderSide(width=2, color="#cccccc"),
+                                BorderSide(width=2, color="#cccccc"),
+                                BorderSide(width=2, color="#cccccc")
+                            ),
+                            prefer_below=False,
+                            wait_duration=250
+                        )
+                    ],
+                    expand=True,
+                    col={"xxl": 4}
+                )
+                for item in cranny.get_items()
+            ]
+        )
         self.widgets.controls = [
             self.clock_widget,
             Container(),
@@ -220,58 +291,114 @@ class HomeController(Controller):
                     self.weekly_widgets
                 ],
                 col={"xxl": 5}
+            ),
+            Column(
+                controls=[
+                    Text("Cranny Rotation", size=20),
+                    self.cranny_widgets
+                ],
+                col={"xxl": 3}
             )
         ]
-        self.update_clock.start()
-        self.update_daily.start()
-        self.update_weekly.start()
+        tasks = [
+            self.update_clock,
+            self.update_daily,
+            self.update_weekly,
+            self.update_cranny,
+        ]
+        for task in tasks:
+            if not task.is_running():
+                task.start()
 
     def setup_events(self):
         ...
 
     @tasks.loop(seconds=1)
     async def update_clock(self):
-        now = datetime.utcnow() - timedelta(hours=11)
-        self.date.value = now.strftime("%Y-%m-%d")
-        self.clock.value = now.strftime("%H:%M:%S")
-        try:
-            await self.date.update_async()
-            await self.clock.update_async()
-        except Exception:
-            ...
-
-    @tasks.loop(seconds=60)
-    async def update_daily(self):
-        await asyncio.sleep(3)
         try:
             now = datetime.utcnow() - timedelta(hours=11)
-            for control in self.daily_widgets.controls:
-                stack = control.content.controls[0].content
-                if int(control.data) == now.weekday():
-                    stack.controls[0].color = None
-                    stack.controls[0].color_blend_mode = None
-                else:
-                    stack.controls[0].color = "black"
-                    stack.controls[0].color_blend_mode = BlendMode.SATURATION
-            await self.daily_widgets.update_async()
+            self.date.value = now.strftime("%Y-%m-%d")
+            self.clock.value = now.strftime("%H:%M:%S")
+            await self.date.update_async()
+            await self.clock.update_async()
+        except AssertionError:
+            ...
         except Exception as e:
             print(e)
 
     @tasks.loop(seconds=60)
+    async def update_daily(self):
+        while True:
+            try:
+                now = datetime.utcnow() - timedelta(hours=11)
+                for control in self.daily_widgets.controls:
+                    stack = control.content.controls[0].content
+                    if int(control.data) == now.weekday():
+                        stack.controls[0].color = None
+                        stack.controls[0].color_blend_mode = None
+                    else:
+                        stack.controls[0].color = "black"
+                        stack.controls[0].color_blend_mode = BlendMode.SATURATION
+                await self.daily_widgets.update_async()
+            except AssertionError:
+                await asyncio.sleep(1)
+                continue
+            except Exception as e:
+                print(e)
+            await asyncio.sleep(3)
+            break
+
+    @tasks.loop(seconds=60)
     async def update_weekly(self):
-        await asyncio.sleep(3)
-        initial = datetime(2020, 3, 30, tzinfo=UTC) - timedelta(hours=11)
-        now = datetime.utcnow() - timedelta(hours=11)
-        week_length = 60 * 60 * 24 * 7
-        weeks = (now.timestamp() - initial.timestamp()) // week_length
-        time_split = weeks / 4
-        time_find = (time_split - int(time_split)) * 4
-        for control in self.weekly_widgets.controls:
-            stack = control.content.controls[0].content
-            if int(control.data) == int(time_find):
-                stack.controls[0].color = None
-                stack.controls[0].color_blend_mode = None
-            else:
-                stack.controls[0].color = "black"
-                stack.controls[0].color_blend_mode = BlendMode.SATURATION
-        await self.weekly_widgets.update_async()
+        while True:
+            try:
+                initial = datetime(2020, 3, 23, tzinfo=UTC) - timedelta(hours=11)
+                now = datetime.utcnow() - timedelta(hours=11)
+                week_length = 60 * 60 * 24 * 7
+                weeks = (now.timestamp() - initial.timestamp()) // week_length
+                time_split = weeks / 4
+                time_find = (time_split - int(time_split)) * 4
+                for control in self.weekly_widgets.controls:
+                    stack = control.content.controls[0].content
+                    if int(control.data) == int(time_find):
+                        stack.controls[0].color = None
+                        stack.controls[0].color_blend_mode = None
+                    else:
+                        stack.controls[0].color = "black"
+                        stack.controls[0].color_blend_mode = BlendMode.SATURATION
+                await self.weekly_widgets.update_async()
+            except AssertionError:
+                await asyncio.sleep(1)
+                continue
+            except Exception as e:
+                print(e)
+            await asyncio.sleep(3)
+            break
+
+    @tasks.loop(seconds=60)
+    async def update_cranny(self):
+        while True:
+            try:
+                now = datetime.utcnow().astimezone(UTC) - timedelta(hours=11)
+                start, end, _, _ = Cranny().current_cranny_timeline()
+                for control in self.cranny_widgets.controls:
+                    card = control.content.controls[0].content
+                    card.border = None
+                    item = card.data
+                    for start, end in item.weeks:
+                        if start < now < end:
+                            card.border = Border(
+                                BorderSide(width=1, color="#ff0000"),
+                                BorderSide(width=1, color="#ff0000"),
+                                BorderSide(width=1, color="#ff0000"),
+                                BorderSide(width=1, color="#ff0000")
+                            )
+                            break
+                await self.cranny_widgets.update_async()
+            except AssertionError:
+                await asyncio.sleep(1)
+                continue
+            except Exception as e:
+                print(e)
+            await asyncio.sleep(3)
+            break
---
+++ utils/data/__init__.py
@@ -0,0 +1,1 @@
+from .shops import Cranny
