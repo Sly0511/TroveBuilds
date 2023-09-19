d7476d1934d02e96e57c035ce3645e04fce43ed7
Sly <darkinleviathan@gmail.com>
2023-09-19 22:00:24 +0100

    Fixed loading without build id

--- controllers/star.py
+++ controllers/star.py
@@ -39,10 +39,12 @@ class StarChartController(Controller):
             self.star_chart = get_star_chart()
             self.map = ResponsiveRow()
             self.star_details = Column(col={"xxl": 2})
-            asyncio.create_task(
-                self.start_with_build_id(self.page.params.get("id", None))
-            )
-            return
+            _id = self.page.params.get("id", None)
+            if _id:
+                asyncio.create_task(
+                    self.start_with_build_id(_id)
+                )
+                return
         self.map.controls.clear()
         self.star_buttons = Stack(
             controls=[
